2588c6389594bd86120801de42e272ef3a7bdee3
poc.hsu <poc.hsu@kyperdata.com>
2015-07-31 14:52:13 +0800

    add attributes for hotspot model

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -1,3 +1,9 @@
 class Hotspot
   include Mongoid::Document
+  include Mongoid::Geospatial
+  field :title, type: String
+  field :url, type: String
+  field :image, type: String
+  geo_field :location
+
 end
